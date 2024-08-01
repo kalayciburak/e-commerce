1d57c56c6167e5171e6300da552bc68052e47bdd
kalayciburak <kalayciburak1996@gmail.com>
2024-08-01 16:03:12 +0300

    Keywords sınıfı, Languages enum'ı kullanılarak daha soyut ve düzenli hale getirildi.

--- common-jpa-package/src/main/java/com/kalayciburak/commonjpapackage/util/constant/Keywords.java
+++ common-jpa-package/src/main/java/com/kalayciburak/commonjpapackage/util/constant/Keywords.java
@@ -1,31 +1,16 @@
 package com.kalayciburak.commonjpapackage.util.constant;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+
+import static com.kalayciburak.commonjpapackage.util.enums.Languages.EN_CREATION;
+import static com.kalayciburak.commonjpapackage.util.enums.Languages.TR_CREATION;
 
 public class Keywords {
     public static final List<String> creationKeywords = new ArrayList<>();
 
-    private static final List<String> TR_CREATION_KEYWORDS = Arrays.asList(
-            "kaydedildi", "kayıt", "kaydet",
-            "oluşturuldu", "oluşturmak", "ekledi", "ekleme",
-            "yeni ekleme", "yeni kayıt", "yeni oluşturma",
-            "eklendi", "ekle", "ekleme işlemi"
-    );
-
-    private static final List<String> EN_CREATION_KEYWORDS = Arrays.asList(
-            "saved", "save", "insert", "created", "create",
-            "added", "add", "adding", "new add", "new record"
-    );
-
-    private static final List<String> FR_CREATION_KEYWORDS = Arrays.asList(
-            "enregistré", "enregistrer", "insérer", "créé", "créer",
-            "ajouté", "ajouter", "ajout", "nouvel ajout", "nouvel enregistrement"
-    );
-
     static {
-        creationKeywords.addAll(TR_CREATION_KEYWORDS);
-        creationKeywords.addAll(EN_CREATION_KEYWORDS);
+        creationKeywords.addAll(TR_CREATION.getKeywords());
+        creationKeywords.addAll(EN_CREATION.getKeywords());
     }
 }
